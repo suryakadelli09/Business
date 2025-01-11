67382b712496afb2a6080ffe46065727805ae74d
SURYA <suryakadelli09@gmail.com>
2025-01-11 12:49:06 +0530

    Cors Changes Updated order of Middlewares

--- Business/Program.cs
+++ Business/Program.cs
@@ -4,47 +4,57 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.FileProviders;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddHttpClient<GeocodingService>();
 
+// Add services to the container.
+builder.Services.AddControllers();
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecificOrigin",
-            builder => builder.WithOrigins("http://localhost:4200")
-                              .AllowAnyHeader()
-                              .AllowAnyMethod());
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
 });
-// Add services to the container.
+builder.Services.AddHttpClient<GeocodingService>();
 
-builder.Services.AddControllers();
+// Add services to the container.
 builder.Services.AddDbContext<BusinessContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Use CORS globally
+app.UseCors("AllowAllOrigins");
+
+app.UseAuthorization();
 
 // Serve static files from the 'uploads' directory
 app.UseStaticFiles(new StaticFileOptions
 {
     FileProvider = new PhysicalFileProvider(
         Path.Combine(app.Environment.ContentRootPath, "uploads")),
-    RequestPath = "/uploads" // Public path to access files
+    RequestPath = "/uploads"
 });
 
-// Configure the HTTP request pipeline.
-//if (app.Environment.IsDevelopment())
-//{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-//}
-
-app.UseHttpsRedirection();
-app.UseCors("AllowSpecificOrigin");
-app.UseStaticFiles();
-
-app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.MapControllers();
 
 app.Run();
+
